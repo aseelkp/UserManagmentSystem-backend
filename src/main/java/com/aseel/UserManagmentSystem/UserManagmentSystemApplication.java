bdff122a308d5ff0e8a4183c190ac2e8ef23a408
aseelkp <aseelaroor10@gmail.com>
2023-09-06 23:52:07 +0530

    Initial commit

---
+++ src/main/java/com/aseel/UserManagmentSystem/UserManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aseel.UserManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagmentSystemApplication.class, args);
+	}
+
+}
